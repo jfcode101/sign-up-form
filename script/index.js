d725ba58cd8c8a41b2dd35a919180dd1d0881bcc
jfcode101 <125421939+jfcode101@users.noreply.github.com>
2024-03-22 09:22:59 -0400

    Initializing the project

---
+++ script/index.js
@@ -0,0 +1,5 @@
+const hThree = document.querySelector("#id-hthree");
+hThree.textContent = `This is not a real online service! You know you need something
+like this in your life to help you realize your deepest dreams.
+`;
+
