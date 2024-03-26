b8507d645920c6c5d683fb66f0ca332f5976fcf6
jfcode101 <125421939+jfcode101@users.noreply.github.com>
2024-03-26 13:27:30 -0400

    Continous improvement

--- README.md
+++ README.md
@@ -1,9 +1,3 @@
 # sign-up-form
+
 A simple sign-up form built using `HTML`, `CSS`, and `JavaScript`
-
-
-
-image
-
-Photo by <a href="https://unsplash.com/@eduschadesoares?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Eduardo Soares</a> on <a href="https://unsplash.com/photos/green-leaf-on-brown-tree-trunk-8yaDRP46do0?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
-  
--- script/index.js
+++ script/index.js
@@ -3,3 +3,22 @@ hThree.textContent = `This is not a real online service! You know you need somet
 like this in your life to help you realize your deepest dreams.
 `;
 
+// const frm = document.querySelector("#id-frm");
+// const inputs = document.querySelectorAll("input");
+// const btnSbmt = document.querySelector("#id-sbmt");
+
+// btnSbmt.addEventListener("click", (e) => {
+//   console.log("we!");
+//   e.preventDefault;
+//   inputs.forEach((inpt) => {
+//     inpt.addEventListener("input", () => {
+//       if (inpt.value === "" || inpt.validity.patternMismatch === true) {
+//         inpt.style.border = "1px solid rgb(160, 3, 3)";
+//         inpt.focus();
+//         console.warn("ayiwe data we!");
+//       } else {
+//         inpt.style.border = "1px solid #45b1e8";
+//       }
+//     });
+//   });
+// });
